45da03c9252143937e09002a2e7dccfefcf455e9
fausto91 <faustocarretero@gmail.com>
2020-07-21 13:50:02 -0500

    RestServer -Usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const uniqueValidator =require('mongoose-unique-validator');
+
+
+let rolesValidos ={
+    values: ['ADMIN_ROLE','SUPER_ROLE'],
+    message:' {VALUE} NO ES UN ROLE VALIDO'
+};
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type:String,
+        required: [true,'El nombre es necesario']  
+    },
+
+    email:{
+        type: String,
+        unique: true,
+        required:[true,'El Email es necesario']
+    },
+
+    password:{
+        type:String,
+        required:[true, ' El Password es necesario']
+    },
+
+    img:{
+        type: String,
+        required:false
+    },
+
+    role:{
+        type:String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+
+    estado:{
+        type:Boolean,
+        default: true
+    },
+
+    google:{
+        type:Boolean,
+        default:false
+    }
+});
+
+// Metodo para que no muestre el objeto de la contraseña en una impresion 
+usuarioSchema.methods.toJSON = function(){
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+
+}
+
+usuarioSchema.plugin(uniqueValidator,{message:'{PATH} DEBE DE SER UNICO'})
+
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,42 +1,27 @@
 require('./config/confing');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const mongoose = require ('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
-app.post('/usuario', function (req, res) {
-    let body= req.body;
 
-    if (body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre fue necesario'
-        })
-    }else {
-        res.json({
-            body
-          });
-        }
-    });
 
-app.put('/usuario/:id', function (req, res) {
-    let id=req.params.id;
+mongoose.connect(process.env.URLDB,
+{ useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:true,useCreateIndex:true},
+ (err,res)=>{
+if(err) throw err;
 
-  res.json({
-      id
-  });
-})
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario')
-})
+console.log('Base de datos ONLINE');
+
+});
  
 app.listen(process.env.PORT, ()=>{
     console.log('Escuchando puerto: ', process.env.PORT);
