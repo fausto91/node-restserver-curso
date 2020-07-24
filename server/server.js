d198107408024f85f23cad880029ff989aded41f
fausto91 <faustocarretero@gmail.com>
2020-07-24 15:37:08 -0500

    update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(require('./routes/index'));
 
 
 mongoose.connect(process.env.URLDB,
-{ useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true},
+{ useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:true,useCreateIndex:true},
  (err,res)=>{
 if(err) throw err;
 
